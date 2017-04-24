e3396408872109f8e79b6a7306815a3c4b1cdfd7
Arturas33 <jasinskis330@gmail.com>
2017-04-24 14:29:55 +0300

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,81 @@ Route::get('/generate-fake-data/persons/{count?}',[
     'uses'=>'PRFakeDataController@generatePersons'
 ]);
 
+Route::get('/clients',[
+    'uses'=>'PRclientsController@index'
+]);
+
+Route::get('/generate-fake-data/clients/{count?}',[
+    'uses'=>'PRFakeDataController@generateClients'
+]);
+Route::get('/clientsPersonType',[
+    'uses'=>'PRclientsPersonTypeController@index'
+]);
+
+Route::get('/generate-fake-data/clientsPersonType/{count?}',[
+    'uses'=>'PRFakeDataController@generateClientsPersonsType'
+]);
+Route::get('/loginName',[
+    'uses'=>'PRLoginNameController@index'
+]);
+
+Route::get('/generate-fake-data/loginName/{count?}',[
+    'uses'=>'PRFakeDataController@generateLoginName'
+]);
+Route::get('/logins',[
+    'uses'=>'PRLoginsController@index'
+]);
+
+Route::get('/generate-fake-data/logins/{count?}',[
+    'uses'=>'PRFakeDataController@generateLogins'
+]);
+
+
+
+Route::get('/Ptypes',[
+    'uses'=>'PRLoginsController@index'
+]);
+
+Route::get('/generate-fake-data/Ptyprs/{count?}',[
+    'uses'=>'PRFakeDataController@generateProjetTypes'
+]);
+Route::get('/Project',[
+    'uses'=>'PRProjectController@index'
+]);
+
+Route::get('/generate-fake-data/Project/{count?}',[
+    'uses'=>'PRFakeDataController@generateProject'
+]);
+
+Route::get('/CPCC',[
+    'uses'=>'PRclientPersonConectedController@index'
+]);
+
+Route::get('/generate-fake-data/CPCC/{count?}',[
+    'uses'=>'PRFakeDataController@generateClientPersonConected'
+]);
+Route::get('/PLCC',[
+    'uses'=>'PRpojectLoginConnectionsController@index'
+]);
+
+Route::get('/generate-fake-data/PLCC/{count?}',[
+    'uses'=>'PRFakeDataController@generatePojectLoginConnections'
+]);
+Route::get('/PTypes',[
+    'uses'=>'PRpersonTypesController@index'
+]);
+
+Route::get('/generate-fake-data/PTypes/{count?}',[
+    'uses'=>'PRFakeDataController@generatePersonTypes'
+]);
+Route::get('/PC',[
+    'uses'=>'PRprojectConnectionsControllers@index'
+]);
+
+Route::get('/generate-fake-data/PC/{count?}',[
+    'uses'=>'PRFakeDataController@generateProjectConnections'
+]);
+
+
+
+
